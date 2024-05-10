9c09a546d63209c3097ca0df2f6228ae1b295c0d
HeniHes <heni.es.paypal@gmail.com>
2024-05-10 16:42:50 +0100

    updates

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b) {
+      a.splice(i, 1); // Remove the element at index i from array a
+    }
+  }
+  return a;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
